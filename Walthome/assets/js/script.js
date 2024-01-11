76424abdf5d2de41b6dd293bfe9ebefcf1dd5d4e
Yağız Can Çakıroğlu <yahizcan34@gmail.com>
2024-01-12 02:17:44 +0300

    Walthome commit V4

--- Walthome/assets/js/script.js
+++ Walthome/assets/js/script.js
@@ -10,3 +10,14 @@ const $header = document.querySelector("[data-header");
 window.addEventListener("scroll", e => {
     $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 });
+
+
+const $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener("click", () => {
+        $toggleBtn.classList.toggle("active");
+    });
+});
+
+// 1.32.16
